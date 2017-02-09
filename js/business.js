c94e42fdcd36e9bf02c331d92cbbe7aa9c0147bb
joshunipro <joshua.adesoba@theuniprogroup.com>
2017-02-09 17:22:57 +0000

    Added recent tweets menu, Changes to tabs and accordion code

--- js/business.js
+++ js/business.js
@@ -1,24 +1,17 @@
 //Tabs JS
-function openTab(evt, cityName) {
-    // Declare all variables
-    var i, tabcontent, tablinks;
+$(document).ready(function(){
+    
+    $('ul.tabs li').click(function(){
+        var tab_id = $(this).attr('data-tab');
 
-    // Get all elements with class="tabcontent" and hide them
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
+        $('ul.tabs li').removeClass('current');
+        $('.tabcontent').removeClass('current');
 
-    // Get all elements with class="tablinks" and remove the class "active"
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
+        $(this).addClass('current');
+        $("#"+tab_id).addClass('current');
+    })
 
-    // Show the current tab, and add an "active" class to the link that opened the tab
-    document.getElementById(cityName).style.display = "block";
-    evt.currentTarget.className += " active";
-}
+})
 
 //Accordion JS
 var acc = document.getElementsByClassName("accordion");
